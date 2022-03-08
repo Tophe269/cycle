5db224b2a235112d9ef77500767f5c02872281d2
Christophe Lachuer <christophe.lachuer@gmail.com>
2022-03-08 23:50:25 +0100

    cycle: board frame

--- src/components/design/Design.tsx
+++ src/components/design/Design.tsx
@@ -1,3 +1,39 @@
-const Design = () => <div />;
+import styled from "styled-components";
+
+import { designBoard } from "../../constants/design-data";
+
+const FrameSet = styled.div`
+  display: flex;
+  flex-direction: line;
+`;
+
+const Frame = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Design = () => (
+  <FrameSet>
+    {designBoard.map(({ slug, icon, title, docs }) => (
+      <Frame key={slug}>
+        <div>
+          {icon} {title}
+        </div>
+        {docs.map(({ id, text, tags, icon: docIcon }) => (
+          <div key={id}>
+            {text}
+            <div>
+              <div>{docIcon}</div>
+              <div>#{id}</div>
+              {tags.map(({ text: tagText }) => (
+                <div key={tagText}>{tagText}</div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </Frame>
+    ))}
+  </FrameSet>
+);
 
 export default Design;
